9177ccd34d3a15c779642489905d5a32dce52ca5
Ed J <mohawk2@users.noreply.github.com>
2017-08-20 04:50:55 +0100

    make standard scalar types

--- lib/GraphQL/Type/Scalar.pm
+++ lib/GraphQL/Type/Scalar.pm
@@ -4,7 +4,9 @@ use 5.014;
 use strict;
 use warnings;
 use Moo;
-use Types::Standard qw(HashRef CodeRef);
+use Types::Standard qw(CodeRef Num Str Bool);
+use GraphQL::Utilities qw(Int32Signed);
+use Exporter qw(import);
 extends qw(GraphQL::Type);
 with qw(
   GraphQL::Role::Input
@@ -15,6 +17,7 @@ with qw(
 );
 
 our $VERSION = '0.02';
+our @EXPORT_OK = qw($Int $Float $String $Boolean $ID);
 
 =head1 NAME
 
@@ -25,7 +28,7 @@ GraphQL::Type::Scalar - GraphQL scalar type
   use GraphQL::Type::Scalar;
   my $int_type = GraphQL::Type::Scalar->new(
     name => 'Int',
-    description => 
+    description =>
       'The `Int` scalar type represents non-fractional signed whole numeric ' .
       'values. Int can represent values between -(2^31) and 2^31 - 1. ',
     serialize => \&coerce_int,
@@ -65,6 +68,82 @@ a Perl entity of the required type, or throws an exception.
 =cut
 
 has parse_literal => (is => 'ro', isa => CodeRef);
+
+=head1 EXPORTED VARIABLES
+
+=head2 $Int
+
+=cut
+
+our $Int = GraphQL::Type::Scalar->new(
+  name => 'Int',
+  description =>
+    'The `Int` scalar type represents non-fractional signed whole numeric ' .
+    'values. Int can represent values between -(2^31) and 2^31 - 1.',
+  serialize => sub { Int32Signed->(@_) },
+  parse_value => sub { Int32Signed->(@_) },
+#  parse_literal => $parse_literal,
+);
+
+=head2 $Float
+
+=cut
+
+our $Float = GraphQL::Type::Scalar->new(
+  name => 'Float',
+  description =>
+    'The `Float` scalar type represents signed double-precision fractional ' .
+    'values as specified by ' .
+    '[IEEE 754](http://en.wikipedia.org/wiki/IEEE_floating_point).',
+  serialize => sub { Num->(@_) },
+  parse_value => sub { Num->(@_) },
+#  parse_literal => $parse_literal,
+);
+
+=head2 $String
+
+=cut
+
+our $String = GraphQL::Type::Scalar->new(
+  name => 'String',
+  description =>
+    'The `String` scalar type represents textual data, represented as UTF-8 ' .
+    'character sequences. The String type is most often used by GraphQL to ' .
+    'represent free-form human-readable text.',
+  serialize => sub { Str->(@_) },
+  parse_value => sub { Str->(@_) },
+#  parse_literal => $parse_literal,
+);
+
+=head2 $Boolean
+
+=cut
+
+our $Boolean = GraphQL::Type::Scalar->new(
+  name => 'Boolean',
+  description =>
+    'The `Boolean` scalar type represents `true` or `false`.',
+  serialize => sub { Bool->(@_) },
+  parse_value => sub { Bool->(@_) },
+#  parse_literal => $parse_literal,
+);
+
+=head2 $ID
+
+=cut
+
+our $ID = GraphQL::Type::Scalar->new(
+  name => 'ID',
+  description =>
+    'The `ID` scalar type represents a unique identifier, often used to ' .
+    'refetch an object or as key for a cache. The ID type appears in a JSON ' .
+    'response as a String; however, it is not intended to be human-readable. ' .
+    'When expected as an input type, any string (such as `"4"`) or integer ' .
+    '(such as `4`) input value will be accepted as an ID.',
+  serialize => sub { Str->(@_) },
+  parse_value => sub { Str->(@_) },
+#  parse_literal => $parse_literal,
+);
 
 __PACKAGE__->meta->make_immutable();
 
