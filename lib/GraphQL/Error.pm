1019abd003d3b1a2fc073dcc8c54615de48db118
Ed J <mohawk2@users.noreply.github.com>
2017-09-17 15:17:25 +0100

    check and coerce methods

--- lib/GraphQL/Error.pm
+++ lib/GraphQL/Error.pm
@@ -34,7 +34,25 @@ has message => (is => 'ro', isa => Str, required => 1);
 
 =head1 METHODS
 
+=head2 is
+
+Is the supplied scalar an error object?
+
 =cut
+
+method is(Any $item) :ReturnType(Bool) { ref $item eq __PACKAGE__ }
+
+=head2 coerce
+
+If supplied scalar is an error object, return. If not, return one with
+it as message.
+
+=cut
+
+method coerce(Any $item) :ReturnType(InstanceOf[__PACKAGE__]) {
+  return $item if $self->is($item);
+  $self->new(message => $item);
+}
 
 __PACKAGE__->meta->make_immutable();
 
