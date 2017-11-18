521faad4709a989c1491418014d8976bb4be2923
taiyoh <sun.basix@gmail.com>
2017-11-18 12:36:05 +0900

    add instruments pool

--- lib/GraphQL/Schema.pm
+++ lib/GraphQL/Schema.pm
@@ -88,6 +88,36 @@ has directives => (
   default => sub { \@GraphQL::Directive::SPECIFIED_DIRECTIVES },
 );
 
+=head2 query_instruments
+
+=cut
+
+has query_instruments => (
+  is => 'ro',
+  isa => ArrayRef[ConsumerOf['GraphQL::Role::Instrument']],
+  default => sub { [] }
+);
+
+=head2 mutation_instruments
+
+=cut
+
+has mutation_instruments => (
+  is => 'ro',
+  isa => ArrayRef[ConsumerOf['GraphQL::Role::Instrument']],
+  default => sub { [] }
+);
+
+=head2 field_instruments
+
+=cut
+
+has field_instruments => (
+  is => 'ro',
+  isa => ArrayRef[ConsumerOf['GraphQL::Role::Instrument']],
+  default => sub { [] }
+);
+
 =head1 METHODS
 
 =head2 name2type
@@ -336,11 +366,26 @@ method register_resolver(
   Str $field_name,
   CodeRef $resolver
 ) :ReturnType(InstanceOf[__PACKAGE__]) {
-  if (my $field = lookup_type({ type => $type_name }, $self->name2type)->fields->{$field_name}) {
-    $field->{resolve} = $resolver;
-  } else {
+  my $type = lookup_type({ type => $type_name }, $self->name2type);
+  my $field = $type->fields->{$field_name};
+  unless ($field) {
     die "Unknown field '$field_name' in $type_name\n";
   }
+  $field->{resolve} = $resolver;
+
+  my @instruments;
+  if ($self->query->name eq $type_name) {
+    @instruments = @{ $self->query_instruments };
+  } elsif ($self->mutation && $self->mutation->name eq $type_name) {
+    @instruments = @{ $self->mutation_instruments };
+  } else {
+    @instruments = @{ $self->field_instruments };
+  }
+
+  for my $inst (@instruments) {
+      $inst->instrument($field, $type);
+  }
+
   return $self;
 }
 
