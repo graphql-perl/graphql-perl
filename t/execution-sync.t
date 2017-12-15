7cf7684b86eb2e484766ab68b65ba830321cc5b1
Ed J <mohawk2@users.noreply.github.com>
2017-12-15 04:20:33 +0000

    add minimal promise-test

---
+++ t/execution-sync.t
@@ -0,0 +1,89 @@
+use lib 't/lib';
+use GQLTest;
+
+my $JSON = JSON::MaybeXS->new->allow_nonref->canonical;
+
+BEGIN {
+  use_ok( 'GraphQL::Schema' ) || print "Bail out!\n";
+  use_ok( 'GraphQL::Execution', qw(execute) ) || print "Bail out!\n";
+  use_ok( 'GraphQL::Type::Object' ) || print "Bail out!\n";
+  use_ok( 'GraphQL::Type::Scalar', qw($String) ) || print "Bail out!\n";
+}
+
+my $schema = GraphQL::Schema->new(
+  query => GraphQL::Type::Object->new(
+    name => 'Query',
+    fields => {
+      syncField => {
+        type => $String,
+        resolve => sub {
+          $_[0];
+        }
+      },
+      asyncField => {
+        type => $String,
+        resolve => sub {
+          my ($root_value, $args, $context, $info) = @_;
+          $info->{promise_code}{resolve}->($root_value);
+        }
+      },
+    }
+  ),
+);
+
+subtest 'test the tests' => sub {
+  my $p = FakePromise->resolve('yo');
+  promise_test($p, ['yo'], '');
+  $p = FakePromise->resolve('yo')->then(sub { shift . 'ga' });
+  is $p->get, 'yoga';
+  is $p->get, 'yoga'; # check can re-get
+  $p = FakePromise->reject("yo\n");
+  promise_test($p, [], "yo\n");
+  $p = FakePromise->reject("f")->catch(sub { shift });
+  promise_test($p, ['f'], "");
+  $p = FakePromise->resolve("yo\n")->then(sub { die shift });
+  promise_test($p, [], "yo\n");
+  $p = FakePromise->reject("f\n")->catch(sub { shift })->then(sub { die shift });
+  promise_test($p, [], "f\n");
+  $p = FakePromise->resolve("yo\n")->then(sub { die shift })->catch(sub { shift });
+  promise_test($p, ["yo\n"], "");
+  my $p = FakePromise->resolve('yo')->then(sub { FakePromise->resolve('y2') });
+  promise_test($p, ["y2"], "");
+  $p = FakePromise->resolve("s\n")->then(sub { FakePromise->reject(shift) });
+  promise_test($p, [], "s\n");
+  $p = FakePromise->resolve("s\n")->then(sub { FakePromise->reject(shift) })->catch(sub { shift });
+  promise_test($p, ["s\n"], "");
+  $p = FakePromise->all(FakePromise->reject("s\n"))->catch(sub { shift });
+  promise_test($p, ["s\n"], "");
+  $p = FakePromise->all('hi', FakePromise->resolve("yo"));
+  promise_test($p, [qw(hi yo)], "");
+  $p = FakePromise->all(
+    'hi',
+    FakePromise->resolve("yo")->then(sub { "$_[0]!" }),
+  )->then(sub { map ucfirst, @_ }),;
+  promise_test($p, [qw(Hi Yo!)], "");
+  $p = FakePromise->all(
+    FakePromise->resolve("hi")->then(sub { "$_[0]!" }),
+    FakePromise->resolve("yo")->then(sub { "$_[0]!" }),
+  )->then(sub { map ucfirst, @_ }),;
+  promise_test($p, [qw(Hi! Yo!)], "");
+  $p = FakePromise->all(
+    FakePromise->all(
+      FakePromise->reject("yo")->then(
+        # simulates rejection that will skip first "then"
+        sub { "$_[0]/" }
+      )->then(
+        # first catch
+        undef,
+        sub { die "$_[0]!\n" },
+      )->then(
+        # second catch
+        undef,
+        sub { die ">$_[0]" },
+      ),
+    ),
+  )->then(undef, sub { map "^$_", @_ }),;
+  promise_test($p, ["^>yo!\n"], "");
+};
+
+done_testing;
--- t/lib/GQLTest.pm
+++ t/lib/GQLTest.pm
@@ -16,7 +16,7 @@ do { eval "use $_; 1" or die $@ } for @IMPORT;
 use Data::Dumper;
 
 our @EXPORT = qw(
-  run_test nice_dump
+  run_test nice_dump fake_promise_code promise_test
 );
 
 sub import {
@@ -36,6 +36,110 @@ sub nice_dump {
   local ($Data::Dumper::Sortkeys, $Data::Dumper::Indent, $Data::Dumper::Terse);
   $Data::Dumper::Sortkeys = $Data::Dumper::Indent = $Data::Dumper::Terse = 1;
   Dumper $got;
+}
+
+sub promise_test {
+  my ($p, $fulfilled, $rejected) = @_;
+  is_deeply [ eval { $p->get } ], $fulfilled;
+  is $@, $rejected;
+}
+
+sub fake_promise_code {
+  +{
+    resolve => sub { FakePromise->resolve(@_) },
+    reject => sub { FakePromise->reject(@_) },
+    all => sub { FakePromise->all(@_) },
+  };
+}
+
+{
+  package FakePromise;
+  # no API-compatible new method, as not going to hook into IO loop etc
+  sub status {
+    # status = undef/'fulfilled'/'rejected'
+    my $self = shift;
+    return $self->{status} unless @_;
+    $self->{status} = shift;
+  }
+  sub steps {
+    # hash-ref with then and/or catch
+    my $self = shift;
+    return $self->{steps} unless @_;
+    die "steps is read-only\n";
+  }
+  sub values {
+    my $self = shift;
+    return @{$self->{values}} unless @_;
+    $self->{values} = [ @_ ];
+  }
+  sub new {
+    my ($class, %attrs) = @_;
+    bless +{ %attrs, steps => [] }, $class;
+  }
+  sub resolve { shift->new(status => 'fulfilled', values => [ @_ ]) }
+  sub reject { shift->new(status => 'rejected', values => [ @_ ]) }
+  sub all { shift->new(status => 'fulfilled', all => [ @_ ]) }
+  sub then {
+    my $self = shift;
+    push @{$self->steps}, +{ then => shift, catch => shift };
+    $self;
+  }
+  sub catch { shift->then(undef, @_) }
+  sub _safe_call { my @r = eval { $_[0]->() }; ($@, @r); }
+  sub _onestep {
+    die "_onestep not in array context" if !wantarray;
+    my ($e, @r) = _safe_call($_[0]);
+    return ('catch', $e) if $e;
+    return ('then', @r) if ref $r[0] ne __PACKAGE__;
+    # real package would deal with still-pending
+    @_ = sub { $r[0]->get }; goto &_onestep; # tail recursion
+  }
+  sub _mapsteps {
+    my ($self, $key, @values) = @_;
+    for (@{$self->steps}) {
+      next if !$_->{$key};
+      ($key, @values) = _onestep(sub { $_->{$key}->(@values) });
+    }
+    $self->status($key eq 'then' ? 'fulfilled' : 'rejected');
+    @values;
+  }
+  sub _finalise {
+    my $self = shift;
+    my @values;
+    if ($self->{all}) {
+      for (@{$self->{all}}) {
+        if (ref $_ ne __PACKAGE__) {
+          push @values, $_;
+          next;
+        }
+        my ($e, @r) = _safe_call(sub { $_->get });
+        if ($e) {
+          $self->status('rejected');
+          @values = ($e);
+          last;
+        }
+        push @values, @r;
+      }
+    } else {
+      @values = $self->values;
+    }
+    if ($self->status eq 'fulfilled') {
+      @values = $self->_mapsteps('then', @values);
+    } elsif ($self->status eq 'rejected') {
+      @values = $self->_mapsteps('catch', @values);
+    }
+    $self->{_settled} = 1;
+    $self->values(@values);
+  }
+  sub get {
+    my $self = shift;
+    $self->_finalise if !$self->{_settled};
+    my @values = $self->values; # must be settled ie fulfilled or rejected
+    die @values if $self->status eq 'rejected';
+    die "Tried to scalar-get but >1 value" if !wantarray and @values > 1;
+    return $values[0] if !wantarray;
+    @values;
+  }
 }
 
 1;
